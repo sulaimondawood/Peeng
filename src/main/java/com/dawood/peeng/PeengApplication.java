8cc1367b0f3f5f98ae1895cbc0f1b054e9fef102
sulaimondawood <sulaimondawod@gmail.com>
2026-05-04 22:13:05 +0100

    first commit

---
+++ src/main/java/com/dawood/peeng/PeengApplication.java
@@ -0,0 +1,13 @@
+package com.dawood.peeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawood/peeng/PeengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawood.peeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
