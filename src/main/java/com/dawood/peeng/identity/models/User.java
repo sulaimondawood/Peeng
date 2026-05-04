72f5f51182ace54799810c7e109f471ec4750946
sulaimondawood <sulaimondawod@gmail.com>
2026-05-04 23:38:26 +0100

    new commit

---
+++ src/main/java/com/dawood/peeng/identity/models/User.java
@@ -0,0 +1,29 @@
+package com.dawood.peeng.identity.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+}
