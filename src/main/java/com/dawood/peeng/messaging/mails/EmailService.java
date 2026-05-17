dc7d93a42ec9b73f14d33d6d55dfc7a94e35b84e
sulaimondawood <sulaimondawod@gmail.com>
2026-05-17 10:43:13 +0100

    new commit

---
+++ src/main/java/com/dawood/peeng/messaging/mails/EmailService.java
@@ -0,0 +1,43 @@
+package com.dawood.peeng.messaging.mails;
+
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+  private final JavaMailSender javaMailSender;
+
+  public void send(String to, String subject, String body) {
+
+    try {
+
+      MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+      helper.setFrom("sulaimond@gmail.com", "Peeng");
+      helper.setTo(to);
+      helper.setSubject(subject);
+      helper.setText(body, true);
+
+      javaMailSender.send(mimeMessage);
+      log.info("Email successfully sent to {}", to);
+
+    } catch (UnsupportedEncodingException | MessagingException e) {
+      log.error("Failed to send email to {}: {}", to, e.getMessage());
+    }
+
+  }
+
+}
