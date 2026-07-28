1b71bfe36ee745bd07028b0c5de837eb5357dd31
sulaimondawood <sulaimondawod@gmail.com>
2026-07-28 20:32:59 +0100

    added newly deployed url to alowed origins

--- src/main/java/com/dawood/peeng/configs/SecurityConfig.java
+++ src/main/java/com/dawood/peeng/configs/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+    configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://127.0.0.1:3000","https://peeng-client.vercel.app"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
